3cf6e50c630b021d5ed400e0caf05674f3718935
medo123 <mrz.m7md@gmail.com>
2024-06-15 04:19:29 +0300

    build_based_simple_linked_list

---
+++ bin/data_structure.dart
@@ -0,0 +1,18 @@
+import 'linked_list.dart';
+
+void main(List<String> arguments) {
+  /// LinkedList
+  print("######################### LINKED LIS START #########################");
+  MrzNode head = MrzNode(value: 1, next: null);
+  MrzLinkedList mrzLinkedList = MrzLinkedList(head: head);
+  mrzLinkedList.addFirst(newNode: MrzNode(value: 0, next: null));
+  mrzLinkedList.addFirst(newNode: MrzNode(value: -1, next: null));
+  mrzLinkedList.display();
+  mrzLinkedList.deleteFirst();
+  mrzLinkedList.display();
+  print("######################### LINKED LIS END #########################");
+
+  List<int> arr = List.filled(5, 0);
+  arr[0] = 0;
+  arr[1] = 1;
+}
---
+++ bin/linked_list.dart
@@ -0,0 +1,50 @@
+class MrzNode{
+  final int value;
+  MrzNode? next;
+  MrzNode({required this.value, required this.next});
+}
+
+
+//TODO: LinkedList contain from two sections: 1- value, 2- address, address is the reference to next node
+//TODO: Last address is being null !!
+//TODO: LinkedList is O(n)
+class MrzLinkedList{
+  MrzNode head;
+  MrzLinkedList({required this.head});
+
+  /// O(1)
+  void addFirst({required MrzNode newNode}){
+    newNode.next = head;
+    head = newNode;
+  }
+
+  /// O(1)
+  // void addEnd({required MrzNode newNode}){
+  //   MrzNode? n = head;
+  //   while(n != null){
+  //     n = n.next;
+  //   }
+  //   n!.next = newNode;
+  //
+  // }
+
+  /// O(1)
+  void deleteFirst(){
+    if(head.next == null){
+      print("can't delete first node, LinkedList has only single node that is default node");
+      return;
+    }
+    head = head.next!;
+  }
+
+  /// O(1/n)
+  void display(){
+    MrzNode? n = head;
+
+    while(n != null){
+      print(n.value);
+      n = n.next;
+    }
+
+  }
+}
